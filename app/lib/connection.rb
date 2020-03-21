b004d8af6fd5084a28d204872d0d82ace5712020
Madan Uprety <madanuprety@Madans-MacBook-Pro.local>
2020-03-21 07:50:10 +0545

    connection.rb class added to establish rest api call to api server

---
+++ app/lib/connection.rb
@@ -0,0 +1,16 @@
+require 'faraday'
+require 'json'
+
+class Connection
+    BASE = 'https://wordsapiv1.p.rapidapi.com/words'
+
+    def self.api
+        Faraday.new(url: BASE) do |faraday|
+            faraday.response :logger
+            faraday.adapter Faraday.default_adapter
+            faraday.headers['Content-Type'] = 'application/json'
+            faraday.headers['x-rapidapi-key'] = ''
+            faraday.headers['x-rapidapi-host'] = 'wordsapiv1.p.rapidapi.com'
+        end
+    end
+end
